db3f7e84a72af6592ec1b73ad561c55295bdc9c7
mpoynina07 <mpoynina07@gmail.com>
2025-10-28 14:17:26 +0100

    Update test_GroveUtrasonicRanger.py

--- test_GroveUtrasonicRanger.py
+++ test_GroveUtrasonicRanger.py
@@ -4,28 +4,39 @@ import RPi.GPIO as GPIO
 import time 
 
 nb_secondes = # à determiner
-TRIG = 23 # envoie un signal ultrasonique (broche sortie)
-ECHO = 24 # reçoit le signal réfléchi (broche entrée)
+BROCHE = 5
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
+GPIO.setup(BROCHE, GPIO.OUT)
+
 
 try :
   while True :
-    GPIO.output(TRIG,True) #lancement du signal
-    time.sleep(0.00001) # pause 0.00001s pour emettre ultrason
-    GPIO.output(TRIG,False) #arret du signal
-    
-  while GPIO.input(ECHO) == 0 :
-    debut = time.time()
-  while GPIO.input(ECHO) == 1 :
-    fin = time.time()
+    GPIO.output(BROCHE, 0)
+        time.sleep(0.002)
+        GPIO.output(BROCHE, 1)
+        time.sleep(0.0001)
+        GPIO.output(BROCHE, 0)
+        
+        GPIO.setup(BROCHE, GPIO.IN)
+        
+        debut = time.time()
+        while GPIO.input(BROCHE) == 0:
+            debut = time.time()
+        while GPIO.input(BROCHE) == 1:
+            fin = time.time()
 
   distance = (fin-debut)*34300/2 # (vitesse du son / 2)
   print("Distance:"+distance+"cm")
 
-  sleep(nb_secondes)
+  sleep(1)
+
+
+except KeyboardInterrupt:
+    
+finally:
+    GPIO.cleanup()
+
 
 
 
