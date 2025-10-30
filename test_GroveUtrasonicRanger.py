27fd651760be783e40500a84fab75b54a60571a5
mpoynina07 <mpoynina07@gmail.com>
2025-10-30 15:25:15 +0100

    Update test_GroveUtrasonicRanger.py

--- test_GroveUtrasonicRanger.py
+++ test_GroveUtrasonicRanger.py
@@ -13,26 +13,29 @@ GPIO.setup(BROCHE, GPIO.OUT)
 try :
   while True :
     GPIO.output(BROCHE, 0)
-        time.sleep(0.002)
-        GPIO.output(BROCHE, 1)
-        time.sleep(0.0001)
-        GPIO.output(BROCHE, 0)
-        
-        GPIO.setup(BROCHE, GPIO.IN)
-        
+    time.sleep(0.002)
+    GPIO.output(BROCHE, 1)
+    time.sleep(0.0001)
+    GPIO.output(BROCHE, 0)
+    
+    GPIO.setup(BROCHE, GPIO.IN)
+    
+    debut = time.time()
+    while GPIO.input(BROCHE) == 0:
         debut = time.time()
-        while GPIO.input(BROCHE) == 0:
-            debut = time.time()
-        while GPIO.input(BROCHE) == 1:
-            fin = time.time()
+    while GPIO.input(BROCHE) == 1:
+        fin = time.time()
+      
+    GPIO.setup(BROCHE, GPIO.OUT)
 
-  distance = (fin-debut)*34300/2 # (vitesse du son / 2)
-  print("Distance:"+distance+"cm")
+    distance = str((fin-debut)*34300/2) # (vitesse du son / 2)
+    print("Distance : "+distance+" cm")
 
-  sleep(n)
+    time.sleep(nb_secondes)
 
 
 except KeyboardInterrupt:
+  print("Programme interrompu par l'utilisateur")
     
 finally:
     GPIO.cleanup()
