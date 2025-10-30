75f6545c713520edb555175261ae4d380862670b
mpoynina07 <mpoynina07@gmail.com>
2025-10-30 16:02:28 +0100

    Update test_Ultrasonic_LED.py

--- test_Ultrasonic_LED.py
+++ test_Ultrasonic_LED.py
@@ -5,7 +5,7 @@ import time
 
 ULTRA = 5
 
-LED_ROUGE = 4 
+LED_ROUGE = 22 
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(ULTRA, GPIO.OUT)
@@ -13,36 +13,39 @@ GPIO.setup(ULTRA, GPIO.OUT)
 GPIO.setup(LED_ROUGE, GPIO.OUT)
 
 
-seuil = # en cm, à determiner
+seuil = # en cm, à determiner (14)
 
-def distance():
-  """ renvoie la mesure de laprofondeur de la boîte depuis le ultrasonic ranger (float)
-  """
-  GPIO.output(ULTRA, 0)
-  time.sleep(0.002)
-  GPIO.output(ULTRA, 1)
-  time.sleep(0.00001)  # pause de 0.00001s pour emettre ultrason
-  GPIO.output(ULTRA, 0)
+try :
+  while True :
+    GPIO.output(ULTRA, 0)
+    time.sleep(0.002)
+    GPIO.output(ULTRA, 1)
+    time.sleep(0.00001)  # pause de 0.00001s pour emettre ultrason
+    GPIO.output(ULTRA, 0)
+    
+    GPIO.setup(ULTRA, GPIO.IN)
   
-  GPIO.setup(ULTRA, GPIO.IN)
-
-  debut = time.time()
-  while GPIO.input(ECHO) == 0 :
     debut = time.time()
-  while GPIO.input(ECHO) == 1 :
-    fin = time.time()
-  
-  return (fin-debut)*34300/2 # calcul de la distance w/vitesse du son
-  
-  try :
-    while True :
-      distance = distance()
-      print("Distance:"+distance+"cm")
-
+    while GPIO.input(ULTRA) == 0 :
+      debut = time.time()
+    while GPIO.input(ULTRA) == 1 :
+      fin = time.time()
+    GPIO.setup(ULTRA, GPIO.OUT)
+    
+    distance = (fin-debut)*34300/2 # calcul de la distance w/vitesse du son
+    
+    
     if distance < seuil :
       GPIO.output(LED_ROUGE, 1)
     else:
       GPIO.output(LED_ROUGE, 0)
+    time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Programme interrompu par l'utilisateur")      
+      
+finally :
+    GPIO.cleanup()
 
 # Ctrl + C pour quitter programme
 
