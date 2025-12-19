347c435b30d6bbc64b48093b315f904ef39fd0e6
mpoynina07 <mpoynina07@gmail.com>
2025-12-19 14:45:24 +0100

    Update test_LedButton.py

--- test_LedButton.py
+++ test_LedButton.py
@@ -9,15 +9,14 @@ pin_button = 22
 GPIO.setup(pin_led, GPIO.OUT)
 GPIO.setup(pin_button, GPIO.IN)
 
-state_led = 0
+etat_led = 0
 
 while True:
-	# connect to pin and define pin number (22)
+	
 	if GPIO.input(pin_button) == 1:
-		state_led = 1 - state_led
+		etat_led = 1 - etat_led
 	#else:
-	#	GPIO.output(pin_led, 1)	
-	GPIO.output(pin_led, state_led)	
+	GPIO.output(pin_led, etat_led)	
 	time.sleep(0.1)
 
 	
